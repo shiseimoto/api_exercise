52c2694dbd6eec87713a685477a12f0b296b6142
shichengyuan <shiseimoto@gmail.com>
2018-06-06 15:44:35 +0800

    add_bootstrap

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,4 +4,16 @@ class CitiesController < ApplicationController
     @cities = City.all
   end
 
+  def update_temp
+    city = City.find(params[:id])
+
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              :params => { :cityname => city.juhe_id, :key => "001c0cbf42318b23274ac21be89e936e" }
+    data = JSON.parse(response.body)
+
+    city.update( :current_temp => data["result"]["sk"]["temp"] )
+
+    redirect_to cities_path
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :cities
+ resources :cities do
+    member do
+     post :update_temp
+    end
+  end
 end
